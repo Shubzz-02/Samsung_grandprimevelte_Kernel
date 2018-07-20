7805b8a28f02fdcd63680c7e825be6238dfd67b4
root <root@localhost.localdomain>
2018-07-20 09:24:01 -0400

    Initial Commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#3 SMP PREEMPT Thu Jun 11 15:39:42 KST 2015"
+#define LINUX_COMPILE_BY "egrace.hwang"
+#define LINUX_COMPILE_HOST "eunhye-Samsung-DeskTop-System"
+#define LINUX_COMPILER "gcc version 4.8 (GCC) "
